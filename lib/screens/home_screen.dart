9d6d98b501dd9e66bb7f0d62723458da5563c6ea
Felipe Souza <felipe.fabricio.m3@gmail.com>
2020-05-28 12:05:34 -0300

    Finalizando a navegação entre tabs.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,23 +6,69 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
+  int _page = 0;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text("Clientes"),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.pinkAccent,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle(color: Colors.white54),
+              ),
+        ),
+        child: BottomNavigationBar(
+            currentIndex: _page,
+            onTap: (p) {
+              _pageController.animateToPage(p,
+                  duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text("Clientes"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart),
+                title: Text("Pedidos"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                title: Text("Produtos"),
+              )
+            ]),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: <Widget>[
+          Container(
+            color: Colors.red,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text("Pedidos"),
+          Container(
+            color: Colors.yellow,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text("Produtos"),
+          Container(
+            color: Colors.green,
           )
         ],
       ),
